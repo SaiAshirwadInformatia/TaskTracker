cffa92faa116a19c1f84cec469e200741cf05e11
Anuj Khairnar <anujkhairnar5@gmail.com>
2016-01-30 12:34:29 +0530

    Add jquery library

--- application/views/head.php
+++ application/views/head.php
@@ -6,16 +6,18 @@
 
   <title>Task Tracker</title>
   
+
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <script src="<?php echo site_url('assets/vendor/jquery/jquery.min.js'); ?>"> </script>
+  <script src="<?php echo site_url('assets/vendor/AdminLTE/dist/js/app.min.js'); ?>" ></script>
+  <script src="<?php echo site_url('assets/vendor/AdminLTE/bootstrap/js/bootstrap.min.js')?>"></script>
+  <script src="<?php echo site_url('assets/vendor/AdminLTE/dist/js/app.min.js')?>"></script>
+
   <link rel="stylesheet" href="<?php echo site_url('assets/vendor/AdminLTE/bootstrap/css/bootstrap.min.css')?>" />
   <link rel="stylesheet" href="<?php echo site_url('assets/vendor/AdminLTE/dist/css/AdminLTE.min.css')?>" />
   <link rel="stylesheet" href="<?php echo site_url('assets/vendor/AdminLTE/dist/css/skins/_all-skins.min.css')?>" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   
   <!-- Ionicons -->
-  
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <script src="<?php echo site_url('assets/vendor/AdminLTE/jquery/dist/jquery.min.js'); ?>" ></script>
-  <script src="<?php echo site_url('assets/vendor/AdminLTE/plugins/jQuery/jQuer-2.1.4.min.js')?>"></script>
-  <script src="<?php echo site_url('assets/vendor/AdminLTE/bootstrap/js/bootstrap.min.js')?>"></script>
-  <script src="<?php echo site_url('assets/vendor/AdminLTE/dist/js/app.min.js')?>"></script>
+
 </head>
